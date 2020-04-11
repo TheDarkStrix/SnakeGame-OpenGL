f4e9f1733220e3cb4418d3b528aae3a943b07031
thedarkstrix <thedarkstrix@gmail.com>
2020-04-11 18:43:06 +0530

    Create a blank window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <GL/glut.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void display(){
+    glutSwapBuffers();
+}
+
 int main(int argc, char** argv) {
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(800,800);
+    glutCreateWindow("SnakeGame OpenGL");
+    glutDisplayFunc(display);
+    glutMainLoop();
 	return 0;
 }
+
+
